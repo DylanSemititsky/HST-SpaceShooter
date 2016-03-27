6ddf32470908f457cf936e8098e400c2098330eb
samsil187 <samsil187@gmail.com>
2016-03-27 16:43:09 -0400

    Boss Scripting
    Added boss aspects AND HOMING LASERS

---
+++ Assets/Boss1Script.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss1Script : MonoBehaviour {
+
+	public GameObject homingLaser;
+
+	public int homingLaserWaveAmount;
+	public float homingLaserFireRate;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			StartCoroutine (WaitToShoot(homingLaserFireRate));
+
+		}
+	}
+	IEnumerator WaitToShoot(float laserRate){
+		
+		for (int i = 0; i < homingLaserWaveAmount; i++) { //fires the laser X amount of times
+			Instantiate (homingLaser, transform.position, Quaternion.identity); //creates laser
+			yield return new WaitForSeconds (laserRate); //waits until laser is shot and shoots again
+		}
+	}
+}
---
+++ Assets/_Scripts/HomingEnemyLaser.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomingEnemyLaser : MonoBehaviour {
+
+	float laserSpeed = 4;
+	public Transform target;
+
+	private Transform myTransform;
+
+	void Awake(){
+		myTransform = transform;
+	}
+	void Start () {
+		GameObject go = GameObject.FindGameObjectWithTag ("Player");
+		target = go.transform;
+		//rotate the projectile to aim at target
+		myTransform.LookAt(target);
+
+	}
+		
+
+	void Update () {
+		//distance moved during laste frame
+		float step = laserSpeed * Time.deltaTime;
+
+		//translate projectile in its forward direction
+		myTransform.Translate (Vector3.forward * step);
+
+		//transform.position = Vector3.MoveTowards (transform.position, target2.transform.position, step );
+		//transform.position = Vector3.Lerp(transform.position, target2.transform.position, step);
+
+	}
+}
