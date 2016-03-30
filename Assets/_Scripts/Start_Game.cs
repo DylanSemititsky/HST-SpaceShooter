29fc1bd19bbc4602bf6ffc65da051832f09fcb5e
DylanSemititsky <dylan.semititsky@gmail.com>
2016-03-29 20:01:38 -0400

    Edited Main Menu Scene
    Added sound effects, flashing, and a delay to the Start button.

--- Assets/_Scripts/Start_Game.cs
+++ Assets/_Scripts/Start_Game.cs
@@ -2,15 +2,37 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-
 public class Start_Game : MonoBehaviour 
 {
+	private AudioSource audioSource;
+	public GameObject explosion;
 
-	void OnTriggerEnter(Collider other)
-	{
-		SceneManager.LoadScene("Asteroid");
+	void Start() {
+		audioSource = GetComponent<AudioSource>();
+
 	}
-		
-		
 
+	IEnumerator OnTriggerEnter(Collider other){
+		audioSource.Play();
+		Instantiate(explosion, transform.position, transform.rotation);
+
+		Renderer rend = GetComponent<Renderer> ();
+		float fadeTime = GameObject.Find ("Fade").GetComponent<Fading>().BeginFade(1);
+
+		for(int i = 1; i <= 10; i++){
+			rend.material.SetColor ("_TintColor", Color.green);
+			yield return new WaitForSeconds (0.1f);
+			rend.material.SetColor ("_TintColor", Color.red);
+			yield return new WaitForSeconds (0.1f);
+		}
+
+		//yield return new WaitForSeconds (fadeTime);
+		SceneManager.LoadScene ("Asteroid");
+	}
+
+	/*IEnumerator LoadScene(){
+		float fadeTime = GameObject.Find ("Fade").GetComponent<Fading> ().BeginFade (1);
+		yield return new WaitForSeconds (fadeTime);
+		SceneManager.LoadScene ("Asteroid");
+	}*/
 }
