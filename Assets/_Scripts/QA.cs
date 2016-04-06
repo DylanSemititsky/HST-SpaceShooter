b011ecec1d5e5d6c59893b6c78f5d25960936ff5
CHICKENB0Y93 <russty93@gmail.com>
2016-04-06 11:47:16 -0400

    Qa Testing
    added button to take testes to a feedback form.

---
+++ Assets/_Scripts/QA.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QA : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GiveFeedBack()
+	{
+		Application.OpenURL("https://drive.google.com/open?id=1qdVBjbcxS_yzn2FEGpu9UoBZo7SQSe-8ZW8ZqiyvibA");
+
+	}
+}
