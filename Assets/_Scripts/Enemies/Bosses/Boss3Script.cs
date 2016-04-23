24b751aae48bf7727b029fc43f262e290f7177e6
samsil187 <samsil187@gmail.com>
2016-04-23 16:49:01 -0400

    Boss3 Updates

--- Assets/_Scripts/Enemies/Bosses/Boss3Script.cs
+++ Assets/_Scripts/Enemies/Bosses/Boss3Script.cs
@@ -6,27 +6,52 @@ public class Boss3Script : MonoBehaviour {
 	//EnterPlaySpace Variables
 	public float enterSpeed;
 	bool hasEntered = false;
+	//Side speed
+	public float sideSpeed;
+	bool dirRight;
+	//grab turret objects
+	GameObject[] turret;
+	GameObject mainTurret;
 
+	public float bottomTurretLaserRate;
+	public float bottomTurretFireRate;
+	public float topTurretLaserRate;
 
-	//Movement Variables
-	public float horizontalSpeed; //bosses horizontal speed
-	public float hAplitude; // the max width range the boss with travel
-	public float originalAplitude;
-	Vector3 tempPosition ;
-	Vector3 originPos;
-	bool setMovement;
-	Vector3 otherTempPos;
+	public float homingLaserWaveAmount;
+	public float homingLaserFireRate;
+	public float homingLaserWaveRate;
+	bool hasFired;
+	bool hasFiredBottomLaser;
+	bool hasFiredTopLaser;
+	public GameObject homingLaser;
+	public GameObject turretLaser;
+	float counter;
+	float bottomLaserCounter;
+	float topLaserCounter;
+
 
 	void Start () {
-		originalAplitude = hAplitude;//SET THESE VARIABLES TO 0
-		hAplitude = 0;
+		
+		turret = new GameObject[4];//set all turret objects on
+		for (int i = 0; i < turret.Length; i++) {
+			turret [i] = gameObject.transform.GetChild (i).gameObject;
+		}
+		mainTurret = GameObject.Find ("Boss3");
 	}
 	
 	void Update () {
 		EnterPlaySpace (); //enter play space
-		BossMovement (); //move left to right
+		//BossMovement (); //move left to right
+		SideToSideMovement();
 
-	
+		CheckHomingLaser ();
+		AttackSequence(); //shoots homing lasers
+
+		CheckBottomLaser ();
+		AttackBottomLaser ();
+
+		AttackTopLaser ();
+		CheckTopLaser ();
 	}
 
 	//enter play space
@@ -35,20 +60,7 @@ public class Boss3Script : MonoBehaviour {
 	//main laser turret
 	//check health
 	//death
-	void BossMovement(){
 
-		tempPosition.x = Mathf.Sin (Time.realtimeSinceStartup * horizontalSpeed) * hAplitude;
-		transform.position = tempPosition;
-
-		if (hasEntered){
-			if (!setMovement) {
-				tempPosition.z += 9.5f;
-				transform.position = tempPosition;
-				setMovement = true;
-			}
-		}
-
-	}
 
 	void EnterPlaySpace(){
 		if (hasEntered == false) {
@@ -64,6 +76,96 @@ public class Boss3Script : MonoBehaviour {
 				//tempPosition = transform.position;
 				//originPos = transform.position;
 			}
+		}
+	}
+	void SideToSideMovement(){
+		if (dirRight) {
+			transform.Translate (Vector3.right * sideSpeed * Time.deltaTime);
+		} else {
+			transform.Translate (-Vector3.right * sideSpeed * Time.deltaTime);
+		}
+
+		if (transform.position.x >= 2.0f) {
+			dirRight = false;
+		}
+		if (transform.position.x <= -2.0f) {
+			dirRight = true;
+		}
+	}
+	IEnumerator ShootHomingLaser(float laserRate){ //create homing laser
+		Vector3 temp = mainTurret.transform.position; // sets the z position to an absolute one
+		temp.y = 0f;
+		//transform.position = temp;
+
+		for (int i = 0; i < homingLaserWaveAmount; i++) { //fires the laser X amount of times
+			
+			Instantiate (homingLaser, transform.position, Quaternion.identity); //creates laser
+
+			yield return new WaitForSeconds (laserRate); //waits until laser is shot and shoots again
+		}
+	}
+	IEnumerator BottomTurretShot(float laserRate){
+		for (int i = 0; i < 2; i++) {
+			Instantiate (turretLaser, turret [i].transform.position, Quaternion.identity);
+		}
+		yield return new WaitForSeconds (laserRate);
+
+	}
+	IEnumerator TopTurretShot(float laserRate){
+		for (int i = 0; i < 2; i++) {
+			Instantiate (turretLaser, turret [i + 2].transform.position, Quaternion.identity);
+		}
+		yield return new WaitForSeconds (laserRate);
+
+	}
+	void CheckBottomLaser(){
+		if (hasEntered == true) {
+			bottomLaserCounter += Time.deltaTime;
+		}
+		if (bottomLaserCounter >= bottomTurretLaserRate) {
+			bottomLaserCounter = 0;
+			hasFiredBottomLaser = false;
+		}
+
+	}
+	void CheckTopLaser(){
+		if (hasEntered == true) {
+			topLaserCounter += Time.deltaTime;
+		}
+		if (topLaserCounter >= topTurretLaserRate) {
+			topLaserCounter = 0;
+			hasFiredTopLaser = false;
+		}
+
+	}
+	void CheckHomingLaser(){
+		if (hasEntered == true) {
+			counter += Time.deltaTime;
+		}
+
+		if (counter >= homingLaserWaveRate) { 
+			counter = 0;
+			hasFired = false;
+		}
+	}
+	void AttackSequence(){
+
+		if (hasEntered == true && hasFired == false) {
+			StartCoroutine (ShootHomingLaser (homingLaserFireRate));
+			hasFired = true;
+		}
+
+	}
+	void AttackBottomLaser(){
+		if (hasEntered == true && hasFiredBottomLaser == false) {
+			StartCoroutine (BottomTurretShot(bottomTurretFireRate));
+			hasFiredBottomLaser = true;
+		}
+	}
+	void AttackTopLaser(){
+		if (hasEntered == true && hasFiredTopLaser == false) {
+			StartCoroutine (TopTurretShot(topTurretLaserRate));
+			hasFiredTopLaser = true;
 		}
 	}
 
