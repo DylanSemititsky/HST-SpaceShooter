9709882ef51aef1534a962aef914d040584de667
CHICKENB0Y93 <russty93@gmail.com>
2016-04-25 11:13:00 -0400

    added pause script
    ESC now pauses the game.

---
+++ Assets/_Scripts/PauseGame.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour {
+
+	bool isPausing = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			if (isPausing == false) 
+			{
+				Time.timeScale = 0f;
+				isPausing = true;
+			} 
+
+			else 
+			
+			{
+				Time.timeScale = 1f; 
+				isPausing = false;
+			}
+				
+		}
+				
+	}
+}
